961fc17c34b9e0e2a1ce29f34e97981bc976119f
Bobbythecoder2034 <kkoala110@gmail.com>
2025-12-12 15:11:25 -0700

    all good

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import {useState, useEffect} from 'react'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 const Map = () => {
+
+
+
+
+
+
+
+
+
   return (
-    <div>Map</div>
+    <div id='map'>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+    
   )
 }
 
