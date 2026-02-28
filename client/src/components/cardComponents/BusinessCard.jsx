1f5580c645efade4d4faef0c242fffdbe3e781b9
Montgomery Mcdonald <halfbloodhero1027@gmail.com>
2026-02-27 21:56:46 -0700

    client/server seperation and scss switch
    made seperate folders for the client and server, and switched us from CSS to SASS.
    
    # TODO: react router

---
+++ client/src/components/cardComponents/BusinessCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FaStar } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
+
+
+const BusinessCard = ({data}) => {
+
+  return (
+    <div className='card' id={data.id}>
+        <section>
+          <div>
+            <h2>{data.businessName}</h2>
+            <h3>{data.missionStatement}</h3>
+            <h4>{data.address}</h4>
+          </div>
+
+          <img className="logo" src={data.logo} alt="" />
+          {/* <p>{data.about}</p> */}
+        </section>
+
+        <section>
+
+          <div>
+                          
+            <p>Ratings: </p>
+
+            {data.rating.sort().reverse().map((val) => {
+              return <span>{val ? <FaStar/> : <CiStar />}</span>
+            })}
+
+          </div>
+          
+          <a href={data.id}>See More</a>
+
+        </section>
+    </div>
+  )
+}
+
+export default BusinessCard 
